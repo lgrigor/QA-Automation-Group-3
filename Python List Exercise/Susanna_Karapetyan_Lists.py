ce4e57a03c77fb8c683c72350c1994cbb56d2d6a
Susik86 <84062837+Susik86@users.noreply.github.com>
2021-08-26 00:27:57 +0400

    Add files via upload

---
+++ Python List Exercise/Susanna_Karapetyan_Lists.py
@@ -0,0 +1,48 @@
+# Python list Exercise
+
+# Exercise 1: Reverse a given list in Python
+# Given List: list1 = [100, 200, 300, 400, 500]
+# Expected output: [500, 400, 300, 200, 100]
+
+
+list1 = [100, 200, 300, 400, 500]
+list1.reverse()
+print(list1)
+
+
+# Exercise 2: Add item 7000 after 6000 in the following Python List
+# Given List: list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# Expected output: [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
+
+# example 1
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+list1[2][2].insert(2, 7000)
+print(list1)
+
+
+# example 2
+list2 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+list2[2][2].append(7000)
+print(list2)
+
+
+# Exercise 3: Given a nested list extend it by adding the sub list ["h", "i", "j"] in such a way that it will look like the following list
+# Given List: list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+# Sub List to be added = ["h", "i", "j"]
+# Expected output: ['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
+
+
+list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+list1[2][1][2].extend(["h", "i", "j"])
+print(list1)
+
+
+# Exercise 4: Given a Python list, find value 20 in the list, and if it is present, replace it with 200. Only update the first occurrence of a value
+# # Given: list1 = [5, 10, 15, 20, 25, 50, 20]
+# # Expected output: list1 = [5, 10, 15, 200, 25, 50, 20]
+
+
+list1 = [5, 10, 15, 20, 25, 50, 20]
+index = list1.index(20)
+list1[index] = 200
+print(list1)
