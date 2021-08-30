48f9052dbe9a8541326a1fca96bb5c182ecfc891
lgrigor <87751350+lgrigor@users.noreply.github.com>
2021-08-30 23:51:46 +0400

    Update and rename Armine_Hunanyan.py.txt to Armine_Hunanyan.py

--- Python List Exercise/Armine_Hunanyan.py
+++ Python List Exercise/Armine_Hunanyan.py
@@ -1,22 +1,22 @@
-List
-Ex1:
+#List
+#Ex1:
 list1 = [100, 200, 300, 400, 500]
 list1.reverse()
 print('Reversed list:', list1)
  
 
-Ex2:
+#Ex2:
 list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
 list1[2][2].insert(1,'7000')
 print(list1[2][2])
  
 
-Ex3:
+#Ex3:
 list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
 list1[2][1][2].extend('hij')
 print(list1)
 
-Ex4:
+#Ex4:
 list1 = [5, 10, 15, 20, 25, 50, 20]
 print(list1[3])
 list1[3] = 200
