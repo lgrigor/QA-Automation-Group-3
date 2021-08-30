682670ee36a397b21f16f8b2e7e2c48bc3caa89d
lgrigor <87751350+lgrigor@users.noreply.github.com>
2021-08-30 23:49:27 +0400

    Update Armine_Hunanyan.py

--- Python Set Exercise/Armine_Hunanyan.py
+++ Python Set Exercise/Armine_Hunanyan.py
@@ -1,29 +1,29 @@
-Set
-Ex1:
+#Set
+#Ex1:
 
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 print( set1.intersection(set2))
 
-Ex2:
+#Ex2:
 
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 print(set1|set2)
 
 
-Ex3:
+#Ex3:
 set1 = {10, 20, 30}
 set2 = {20, 40, 50}
 print(set1.difference(set2))
 
  
-Ex4:
+#Ex4:
 set1 = {10, 20, 30, 40, 50}
 Expected output: {40, 50}
 print(set2.difference(set1))
 
-Ex5:
+#Ex5:
 
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
