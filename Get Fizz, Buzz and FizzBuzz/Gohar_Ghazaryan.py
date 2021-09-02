5a7e98faae9b773535bd90c4230ee7a610ed8fe5
ghazaryangohar <89595559+ghazaryangohar@users.noreply.github.com>
2021-09-02 23:53:06 +0400

    Add files via upload

---
+++ Get Fizz, Buzz and FizzBuzz/Gohar_Ghazaryan.py
@@ -0,0 +1,10 @@
+for num in range(1,50):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
