e83181a08a40e11ee860e181f2421653d5337bf1
lgrigor <87751350+lgrigor@users.noreply.github.com>
2021-09-05 19:48:02 +0400

    Delete Examples from Slides/Lesson 7: Python Collecti directory

--- Examples from Slides/Lesson 7: Python Collecti/Counter.py
+++
@@ -1,7 +0,0 @@
-import collections
-
-count = collections.Counter('soooooommmmmeeeee sssttttttrrrrriiiiinnnnnnnggggg')
-count = collections.Counter(['a','a','a','a','a','b','b','b','b'])
-count.subtract('o'*5000)
-print(count['o'])
-
--- Examples from Slides/Lesson 7: Python Collecti/OrderedDict.py
+++
@@ -1,9 +0,0 @@
-import collections
-
-d = collections.OrderedDict({'a': 1})
-d['b'] = 2
-d['c'] = 3
-print(d)
-
-d.move_to_end('a')
-print(d)
