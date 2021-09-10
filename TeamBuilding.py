0dd5d0595f9e175399d24b9a98365e1b60cde512
lgrigor <87751350+lgrigor@users.noreply.github.com>
2021-09-10 23:15:23 +0400

    Create TeamBuilding.py

---
+++ TeamBuilding.py
@@ -0,0 +1,12 @@
+import time as tm
+date = '11․09․2021'
+place = 'Երևան Պանդոկ'
+while date == '11․09․2021':
+     time = tm.strftime("%H:%M", tm.localtime())
+     if time == '17:00':
+          print('Դու արդեն պետքա ' + place + '-ում լինեիր')
+     elif time > '17:00' and 'ունեք գործեր':
+          print('կարող եք նաև միանալ ավելի ուշ')
+     else:
+          print('լիկվիդ!')
+          exit(1)
