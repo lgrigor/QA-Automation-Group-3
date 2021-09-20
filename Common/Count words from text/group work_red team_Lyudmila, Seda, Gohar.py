9695060f24d4355baebf7f8f3be102d24f1d9172
Lyudmila Barseghyan <90136896+lyud-mila@users.noreply.github.com>
2021-09-20 19:03:16 +0400

    Add files via upload

---
+++ Common/Count words from text/group work_red team_Lyudmila, Seda, Gohar.py
@@ -0,0 +1,49 @@
+import re
+
+outfile = open("/home/lyuka/Downloads/text.txt", "r")
+text = outfile.readlines()
+
+
+counts = dict()
+
+for line in text:
+    print(line)
+    
+
+    words = re.findall(r'\w{2,}', line)
+    print(words)
+
+    for word in words:
+        print(word)
+        word = word.lower()
+
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+
+
+        
+print(counts)
+
+list_new = sorted(list(counts.keys()))
+
+for key in list_new:
+    print(key, ":", counts[key])
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
