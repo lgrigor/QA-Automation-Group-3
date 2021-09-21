8e4b6d56c0e0f1f5c199df643cbb36fe42f13e9f
AramGevorgian <76686250+AramGevorgian@users.noreply.github.com>
2021-09-21 15:58:34 +0400

    Update Aram_Gevorgyan_BankAccount.py

--- Object Oriented Programming/Aram_Gevorgyan_BankAccount.py
+++ Object Oriented Programming/Aram_Gevorgyan_BankAccount.py
@@ -12,25 +12,28 @@ class BankAccount:
 
   def withdrawal(self):
     balance2 = float(input('Enter your withdrawal: '))
-    self.balance -= balance2
-    print('Your balance after withdrawal is:')
-    print(self.balance)
+    if balance2 < self.balance:
+      self.balance -= balance2
+      print('Your balance after withdrawal is:')
+      print(self.balance)
+    else:
+      print("You don't have enough money")
 
   def bankFees(self):
     balance3 = 0.95*(self.balance)
-    print('Your balance after bankFee is: ', + balance3)
     self.balance -= balance3
     print('Your bankFee is:')
     print(self.balance)
+    print('Your balance after bankFee is: ', + balance3)
     self.balance = balance3
 
   def display(self):
     print('Information about card holder:')
     print(self.accountNumber, self.name, self.balance)
 
-bankAccount = BankAccount(1021401, 'AramG', 1111)
+
+bankAccount = BankAccount("accountNumber is: 1021401,", "cardHolderName: Aram,", 0)
 bankAccount.deposit()
 bankAccount.withdrawal()
 bankAccount.bankFees()
 bankAccount.display()
-
