5b862f7d257c54eafcdca5126331adaf3b9dbaf1
Nare Manukyan <nare.manukyan.66@gmail.com>
2021-09-23 10:41:07 +0400

    Update NareManukyan.py

--- Test the number is prime or not/NareManukyan.py
+++ Test the number is prime or not/NareManukyan.py
@@ -5,9 +5,10 @@ def get_prime_number(num):
            if num % n == 0:
                print(num, "is not a prime number")
                return False
-
+     
+   else:
+      return True
    print(num, "is a prime number")
-   return True
 
 get_prime_number(int(input("Enter a number: ")))
 
