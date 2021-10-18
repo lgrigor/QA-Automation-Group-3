8b6c574eb505f4210fe1a9a44f35b54af795a369
lelencho <89686550+lelencho@users.noreply.github.com>
2021-10-18 17:18:13 +0400

    Add files via upload

---
+++ Python Tuple Exercise/Yelena_Manukyan.py
@@ -0,0 +1,56 @@
+tuple1 = (10, 20, 30, 40, 50)
+x = list(tuple1)
+x.reverse()
+print(x)
+
+
+
+tuple = ("Orange", [10, 20, 30], (5, 15, 25))
+print (tuple[1][1])
+
+
+a = (10, 20, 30, 40)
+x, y, z, w = a
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+
+
+
+a = (11, 22, 33, 44, 55, 66)
+x = a[3 : 5]
+print (x)
+
+
+
+tuple2 = (11, [22, 33], 44, 55)
+tuple2[1][0] = 222
+print(tuple2)
+
+
+
+tuple2 = (50, 10, 60, 70, 50)
+x = tuple2.count(50)
+print(x)
+
+
+
+
+x = (50, )
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
