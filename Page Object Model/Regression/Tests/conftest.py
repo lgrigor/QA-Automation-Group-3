971acc79f023b1cb394c702c3a481539788671ed
lgrigor <87751350+lgrigor@users.noreply.github.com>
2021-10-23 14:02:04 +0400

    Add files via upload

---
+++ Page Object Model/Regression/Tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(params=['chrome'])
+def setUp(request):
+    if request.param == 'chrome':
+        chrome_options = Options()
+        chrome_options.add_argument('--log-level=3')
+        chrome_options.add_argument('--start-maximized')
+        browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+    elif request.param == 'firefox':
+        assert False, 'Firefox browser are not supported yet!'
+
+    return browser
