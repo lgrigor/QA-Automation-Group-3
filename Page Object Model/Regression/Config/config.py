8fce9e494562d908b3e14cd6f5abd0d08e9180ef
lgrigor <87751350+lgrigor@users.noreply.github.com>
2021-10-23 14:53:35 +0400

    Add files via upload

---
+++ Page Object Model/Regression/Config/config.py
@@ -0,0 +1,12 @@
+class TestData():
+
+    #Drivers
+    CHROME_PATH = r'CHROME_PATH_TO_DRIVER'
+    FIREFOX_PATH = r'FIREFOX_PATH_TO_DRIVER'
+    
+    #Data
+    VALID_EMAIL = 'test@gmail.com'
+    VALID_PASSWORD = '^&*tESt123!@#'
+    
+    INVALID_EMAIL = 'testgmail.com'
+    INVALID_PASSWORD = 'invalidpassword'
