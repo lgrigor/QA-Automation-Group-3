edf09cda1ace41cd4eaa0ab9e6eab6793bda6562
Lyudmila Barseghyan <90136896+lyud-mila@users.noreply.github.com>
2021-11-01 10:13:43 +0400

    Update Lyudmila_Barseghyan_Log In.py

--- Log In/Lyudmila_Barseghyan_Log In.py
+++ Log In/Lyudmila_Barseghyan_Log In.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 browser = webdriver.Chrome('chromedriver')
 browser.get("https://www.saucedemo.com/")
 
-browser.find_element_by_id("user-name").send_keys("standard_user")
-browser.find_element_by_id("password").send_keys("secret_sauce")
-browser.find_element_by_name("login-button").click()
+find_user_name = browser.find_element(By.ID, "login_credentials")
+find_user_password = browser.find_element(By.CLASS_NAME, "login_password")
+user_name = browser.find_element(By.ID, "user-name").send_keys(find_user_name.text.split('\n')[1])
+password = browser.find_element(By.ID, "password").send_keys(find_user_password.text.split('\n')[1])
+browser.find_element(By.ID, "login-button").click()
